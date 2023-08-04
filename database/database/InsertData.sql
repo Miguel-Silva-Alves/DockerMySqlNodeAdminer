f497b3d8fd6f74033a48a55bd87bb52f71c7008c
Estevan Sousa <estevanmartins03@gmail.com>
2023-08-04 16:45:52 -0300

    :sparkles:feat:Chapter8-using variables

--- database/database/InsertData.sql
+++ database/database/InsertData.sql
@@ -148,6 +148,79 @@ grant select on study.* to 'Miguel157'@'%';
 
 show grants for 'Miguel123'@'%';
 
+-- CHAPTER 8 // USING VARIABLES
+
+-- SETTING VARIABLES
+set @var_string = 'my_var';
+set @var_num = 2; -- set @var_num = '2';
+set @var_date = '2004-04-26';
+
+select @var_num = 2; -- retorna se é verdadeiro ou falso com 0 ou 1
+select @var_num := 2; -- atribui o valor da variavel novamente com o valor informado
+
+set @start_date = '2012/12/12';
+set @end_datte = '2013/02/13';
+
+set @start_yearmonth = (select extract(year_month from @start_date));
+set @end_yearmonth = (select extract(year_month from @end_date));
+
+-- put the partitions into a variable
+select group_concat(partition_name) from information_schema.partitions p
+        where table_name = 'partitioned_table'
+        and substring_index(partition_name,'P',-1) between @start_yearmonth and @end_yearmonth
+        into @partitions;
+
+
+-- put the query in a variable. I need to do this, because mysql
+--did not recognize my variable as a variable in that position. I need
+--to contact the value of the variable together with the rest of the
+--query and then execute it as a stmt
+set @query = concat('create table part_of_partitioned_table (primary key(id))
+        select partitioned_table.*
+        from partitioned_table partition(', @partitions, ')
+        join users u using(user_id)
+        where date(partitioned_table.date) between ', @start_date, 'and', @end_date);
+
+-- prepare the statement from @query
+prepare stmt from @query;--don't works
+
+-- i tried execute this commands bellow because the command above don't worked
+SET @partitions = COALESCE(@partitions, ''); -- Define um valor padrão se @partitions for nulo
+SET @start_date = COALESCE(@start_date, ''); -- Define um valor padrão se @start_date for nulo
+SET @end_date = COALESCE(@end_date, '');
+
+-- Remover espaços em branco extras, se necessário
+SET @partitions = TRIM(@partitions);
+SET @start_date = TRIM(@start_date);
+SET @end_date = TRIM(@end_date);
+
+--drop table
+drop table if exists tech.part_of_partitioned_table;
+--create table using statement
+execute stmt;
+
+create table team_person as select 'A' team, 'John' person
+        union all select 'B' team, 'Isabela' person
+        union all select 'A' team, 'Haroldo' person
+        union all select 'A' team, 'Daniel' person
+        union all select 'B' team, 'Danilo' person
+        union all select 'B' team, 'Gabriel' person;
+
+-- don't works
+select @row_no := @row_no+1 as row_number, team, person
+        from team_person, (select @row_no := 0) t;
+-- don't works too
+set @row_no := 0;
+select @row_no := @row_no + 1 as row_number, team, person
+from team_person;
+
+
+-- CHAPTER 9 // COMMENTS
+-- WORKS(--, #,/* */)
+
+
+-- CHAPTER 10 // INSERT
+
 
 
 
